a9c32534b2886dd6041791f41615a253ab8fb8fe
Ryan Scott Taylor <58642765+ryantaylorza@users.noreply.github.com>
2025-01-16 16:12:32 -0700

    Update ucyncu.c

--- drivers/ucyncu/ucyncu.c
+++ drivers/ucyncu/ucyncu.c
@@ -1,1 +1,209 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/soundcard.h>
+#include <linux/alsa.h>
+#include <linux/slab.h> 
+#include <linux/inet.h> 
+#include <linux/netdevice.h>
+#include <linux/socket.h> 
 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Ryan S Taylor");
+MODULE_DESCRIPTION("UCYNCU Audio Driver");
+MODULE_VERSION("0.1");
+
+#define UCYNCU_DRIVER_DATE __DATE__
+
+/* This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+// Declare the ucyncu_pcm device
+static struct snd_pcm *ucyncu_pcm; 
+
+static int ucyncu_pcm_open(struct snd_pcm_substream *substream) {
+    printk(KERN_INFO "UCYNCU: PCM opened\n");
+
+    // ... (Allocate resources) ...
+
+    // Create a TCP socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        printk(KERN_ERR "UCYNCU: Error creating socket\n");
+        return -1;
+    }
+
+    // Set up server address structure
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = in_aton("192.168.1.100"); // Replace with server IP
+    server_addr.sin_port = htons(50007); // Replace with server port
+
+    // Connect to the server
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        printk(KERN_ERR "UCYNCU: Error connecting to server\n");
+        return -1;
+    }
+
+    // Store the socket file descriptor in the substream's private data
+    substream->private_data = (void *)sockfd;
+
+    return 0;
+}
+
+static int ucyncu_pcm_close(struct snd_pcm_substream *substream) {
+    printk(KERN_INFO "UCYNCU: PCM closed\n");
+
+    // Get the socket file descriptor
+    int sockfd = (int)substream->private_data;
+
+    // Close the socket
+    close(sockfd);
+
+    return 0;
+}
+
+static int ucyncu_pcm_hw_params(struct snd_pcm_substream *substream,
+                                 struct snd_pcm_hw_params *hw_params) {
+    int err;
+    unsigned int rate = 48000; // Default sample rate
+    snd_pcm_format_t format = SNDRV_PCM_FORMAT_S32_LE; // Default sample format
+
+    printk(KERN_INFO "UCYNCU: Setting hardware parameters\n");
+
+    // Set the access type (SNDRV_PCM_ACCESS_RW_INTERLEAVED for interleaved samples)
+    err = snd_pcm_hw_params_set_access(substream, hw_params, SNDRV_PCM_ACCESS_RW_INTERLEAVED);
+    if (err < 0) {
+        printk(KERN_ERR "UCYNCU: Error setting access type\n");
+        return err;
+    }
+
+    // Set the sample format
+    err = snd_pcm_hw_params_set_format(substream, hw_params, format);
+    if (err < 0) {
+        printk(KERN_ERR "UCYNCU: Error setting sample format\n");
+        return err;
+    }
+
+    // Set the sample rate
+    err = snd_pcm_hw_params_set_rate_near(substream, hw_params, &rate, 0);
+    if (err < 0) {
+        printk(KERN_ERR "UCYNCU: Error setting sample rate\n");
+        return err;
+    }
+
+    // Set the number of channels
+    err = snd_pcm_hw_params_set_channels(substream, hw_params, 24); // 24 channels
+    if (err < 0) {
+        printk(KERN_ERR "UCYNCU: Error setting channels\n");
+        return err;
+    }
+
+    // ... (Set other parameters like buffer size, period size, etc.) ...
+
+    return 0;
+}
+
+static int ucyncu_pcm_hw_free(struct snd_pcm_substream *substream) {
+    printk(KERN_INFO "UCYNCU: Freeing hardware resources\n");
+    // ... (Release hardware resources) ...
+    return 0;
+}
+
+static int ucyncu_pcm_prepare(struct snd_pcm_substream *substream) {
+    printk(KERN_INFO "UCYNCU: Preparing stream\n");
+    // ... (Prepare the stream for playback/capture) ...
+    return 0;
+}
+
+static int ucyncu_pcm_trigger(struct snd_pcm_substream *substream, int cmd) {
+    printk(KERN_INFO "UCYNCU: Triggering stream\n");
+    // ... (Start/stop the audio stream) ...
+    return 0;
+}
+
+static snd_pcm_uframes_t ucyncu_pcm_pointer(struct snd_pcm_substream *substream) {
+    // ... (Return the current position in the audio buffer) ...
+    return 0; 
+}
+
+static int ucyncu_pcm_read(struct snd_pcm_substream *substream, void *buf, snd_pcm_uframes_t size) {
+    printk(KERN_INFO "UCYNCU: Reading audio data\n");
+    // ... (Receive audio data from the server and copy to 'buf') ...
+
+    // Get the socket file descriptor
+    int sockfd = (int)substream->private_data;
+
+    // Receive audio data from the server
+    int bytes_received = recv(sockfd, buf, size, 0); 
+    if (bytes_received < 0) {
+        printk(KERN_ERR "UCYNCU: Error receiving data\n");
+        return -1;
+    }
+
+    return 0; // Return the number of frames read
+}
+
+static int ucyncu_pcm_write(struct snd_pcm_substream *substream, const void *buf, snd_pcm_uframes_t size) {
+    printk(KERN_INFO "UCYNCU: Writing audio data\n");
+    // ... (Send audio data from 'buf' to the server) ...
+
+    // Get the socket file descriptor
+    int sockfd = (int)substream->private_data;
+
+    // Send audio data to the server
+    int bytes_sent = send(sockfd, buf, size, 0);
+    if (bytes_sent < 0) {
+        printk(KERN_ERR "UCYNCU: Error sending data\n");
+        return -1;
+    }
+
+    return 0; // Return the number of frames written
+}
+
+static struct snd_pcm_ops ucyncu_pcm_ops = {
+    .open = ucyncu_pcm_open,
+    .close = ucyncu_pcm_close,
+    .ioctl = snd_pcm_lib_ioctl,
+    .hw_params = ucyncu_pcm_hw_params,
+    .hw_free = ucyncu_pcm_hw_free,
+    .prepare = ucyncu_pcm_prepare,
+    .trigger = ucyncu_pcm_trigger,
+    .pointer = ucyncu_pcm_pointer,
+    .read = ucyncu_pcm_read,
+    .write = ucyncu_pcm_write,
+};
+
+static int __init ucyncu_init(void) {
+    int ret;
+    printk(KERN_INFO "UCYNCU: Driver loaded (Built on %s)\n", UCYNCU_DRIVER_DATE);
+
+    // ALSA device registration 
+    ret = snd_pcm_new(SNDRV_PCM_STREAM_PLAYBACK, "UCYNCU", 0, 1, 0, &ucyncu_pcm); 
+    if (ret < 0) {
+        printk(KERN_ERR "UCYNCU: Error registering PCM device\n");
+        return ret;
+    }
+
+    strcpy(ucyncu_pcm->name, "UCYNCU");
+    ucyncu_pcm->private_data = NULL; 
+    snd_pcm_set_ops(ucyncu_pcm, SNDRV_PCM_STREAM_PLAYBACK, &ucyncu_pcm_ops);
+
+    printk(KERN_INFO "UCYNCU: PCM device registered\n");
+    return 0;
+}
+
+static void __exit ucyncu_exit(void) {
+    printk(KERN_INFO "UCYNCU: Driver unloaded\n");
+
+    // ALSA device unregistration
+    snd_pcm_free(ucyncu_pcm); 
+
+    printk(KERN_INFO "UCYNCU: PCM device unregistered\n");
+}
+
+module_init(ucyncu_init);
+module_exit(ucyncu_exit);
